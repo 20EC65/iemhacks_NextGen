e43f6ba8c5a66817c2cab1eafa06106283173b80
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-08-26 18:54:58 +0530

    Backend: Added Models

---
+++ Backend/src/model/BloodReport.js
@@ -0,0 +1,26 @@
+import mongoose, { Mongoose, mongo } from 'mongoose';
+
+const bloodReportSchema = new mongoose.Schema({
+    RBC:{
+        type:String
+    },
+    WBC:{
+        type: String
+    },
+    Platelets: {
+        type: String 
+    },
+    hemoglobin:{
+        type: String
+    },
+    user:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ] 
+}, {timestamps: true});
+
+const BloodReport = mongoose.model('BloodTest', bloodReportSchema);
+
+export default BloodReport;
